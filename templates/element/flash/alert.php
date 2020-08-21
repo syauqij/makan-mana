1b46e566d91f3b8c5d6ed79b2781b3cb3479d45e
Muhamad Syauqi Jamil <syauqi.j@outlook.com>
2020-08-22 02:59:37 +0800

    Fix flash message variable not defined

--- templates/element/flash/alert.php
+++ templates/element/flash/alert.php
@@ -10,7 +10,7 @@ if (!isset($params['escape']) || $params['escape'] !== false) {
 ?>
 
 <div class="col-12 mt-2 alert alert-<?= h($params['type']) ?> alert-dismissible fade show" role="alert">
-    <?= h($message) ?>: <?= h($params['name']) ?>
+    <?= h($message) ?> <b><?= h(isset($params['name'])) ?> </b>
     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
         <span aria-hidden="true">&times;</span>
     </button>
